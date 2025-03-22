1acfcd2c7bc33ef68cc3bce74f661a9015d7df24
Alexander Koller <akoller@gmail.com>
2025-03-22 20:27:09 +0100

    removed WASM plugin from packaging process

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -12,11 +12,13 @@ fi
 RELEASE_DIR=release/preview/blinky/$VERSION
 PLUGIN_DIR=plugin/blinky/plugin
 
-# Check that WASM is up to date.
 
-pushd $PLUGIN_DIR
-cargo build --target wasm32-unknown-unknown --release
-popd
+### WASM no longer needed for blinky >= 0.2.0.
+# # Check that WASM is up to date.
+
+# pushd $PLUGIN_DIR
+# cargo build --target wasm32-unknown-unknown --release
+# popd
 
 
 # Put together release
@@ -27,7 +29,7 @@ mkdir -p $RELEASE_DIR
 cp blinky.typ $RELEASE_DIR/lib.typ
 cp README.md $RELEASE_DIR/
 cp LICENSE $RELEASE_DIR/
-cp $PLUGIN_DIR/target/wasm32-unknown-unknown/release/blinky.wasm $RELEASE_DIR/
+# cp $PLUGIN_DIR/target/wasm32-unknown-unknown/release/blinky.wasm $RELEASE_DIR/
 
 # replace version in typst.toml
 sed "s/VERSION/$VERSION/g" typst-template.toml > $RELEASE_DIR/typst.toml
